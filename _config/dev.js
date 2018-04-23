672499869b3c2da6e761717a44188b9b50d47323
Slava Bezgachev <bezgachev@gmail.com>
2018-04-23 15:14:11 +0600

    Pre-eject commit

---
+++ _config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoUri: "mongodb://konfetka:konfetkapass@ds113098.mlab.com:13098/konfetka-dev",
+  redisUri: "redis://127.0.0.1",
+  cookieKey: "asdasdasdasd"
+};
---
+++ _config/test.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoUri: "mongodb://127.0.0.1/konfetka-test",
+  redisUri: "redis://127.0.0.1",
+  cookieKey: "asdasdasdasd"
+};
